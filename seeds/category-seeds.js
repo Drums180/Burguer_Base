c071ca575ceda57cae8ce064499e0e27b829ba4a
Drums180 <daviddomor180@hotmail.com>
2023-03-15 19:43:06 -0600

    final seed commit

--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -2,25 +2,35 @@ const { Category } = require('./models');
 
 const categoryData = [
   {
-    name: 'Breads',
+    name: 'Bread',
+    ingredient_ids: [1],
   },
   {
     name: 'Vegetables',
+    ingredient_ids: [2, 6, 9, 10],
   },
   {
-    name: 'Meats',
+    name: 'Meat',
+    ingredient_ids: [3, 4],
   },
   {
     name: 'Dairy',
+    ingredient_ids: [5],
   },
   {
     name: 'Sauces',
+    ingredient_ids: [7, 11],
   },
   {
-    name: 'Fruits',
+    name: 'Fruit',
+    ingredient_ids: [8],
   },
 ];
 
-const seedCategories = () => Category.bulkCreate(categoryData);
+const seedCategories = async () => {
+  await Category.bulkCreate(categoryData, {
+    include: 'ingredients',
+  });
+};
 
 module.exports = seedCategories;
