879bfecec5d3e17f77424274bd69972d71152780
David Dominguez <118247139+Drums180@users.noreply.github.com>
2023-03-22 14:00:06 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
-const express = require ("express")
-const exphbs = require ("express-handlebars")
-const path = require ("path")
-const session = require ("express-session")
-const sequelize = require ("./config/connection")
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const session = require('express-session');
+const sequelize = require('./config/connection');
 
-const app = express ()
-// const sesh ={} this will be used for the loggin with exprees library
-const hbs = exphbs.create({})
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.engine("hbs", hbs.engine)
-app.set ("view engine","handlebars") 
-app.use(express.json()) 
+const hbs = exphbs.create({});
 
+// Middleware
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+// AUTHENTICATION PENDING
+const crypto = require('crypto');
+const secretKey = crypto.randomBytes(64).toString('hex');
 
+app.use(session({
+  secret: secretKey,
+  resave: false,
+  saveUninitialized: false
+}));
 
-sequelize.sync({ force }).then(() => {
-  app.listen(3001, () => {
-    console.log(`App listening on port ${PORT}!`);
-  });
+// Routes
+const routes = require('./routes');
+app.use(routes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
+
