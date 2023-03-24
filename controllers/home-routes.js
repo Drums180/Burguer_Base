ca6a6eb5932cf7d73d4129e7b23d203ff76ad371
Fabrizio Treviño <fabriziotrevino@gmail.com>
2023-03-24 13:53:13 -0600

    updaded handlebars menu

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,53 +3,56 @@ const { Ingredients, Category, Suppliers } = require('../models');
 
 // Here is where we provide hardcoded data to render dynamically
 const hamburgers = [
-    {
-      burger_name: 'Cheese Burger',
-      description: 'Burger with cheese, lettuce, tomato, and onion',
-      ingredients: {
-        bread: 0.2,
-        meat: 0.2,
-        cheese: 0.1,
-        lettuce: 0.05,
-        tomato: 0.05,
-        onion: 0.05,
-        ketchup: 0.02,
-        mayo: 0.02,
-      },
+  {
+    burger_name: 'Cheese Burger',
+    description: 'Burger with cheese, lettuce, tomato, and onion',
+    photo: '/public/assets/cheese.jpg',
+    ingredients: {
+      bread: 0.2,
+      meat: 0.2,
+      cheese: 0.1,
+      lettuce: 0.05,
+      tomato: 0.05,
+      onion: 0.05,
+      ketchup: 0.02,
+      mayo: 0.02,
     },
-    {
-      burger_name: 'Classic Burger',
-      description: 'Burger with lettuce, tomato, and onion',
-      ingredients: {
-        bread: 0.2,
-        meat: 0.2,
-        lettuce: 0.05,
-        tomato: 0.05,
-        onion: 0.05,
-        ketchup: 0.02,
-        mayo: 0.02,
-      },
+  },
+  {
+    burger_name: 'Classic Burger',
+    description: 'Burger with lettuce, tomato, and onion',
+    photo: '/public/assets/classic.jpg',
+    ingredients: {
+      bread: 0.2,
+      meat: 0.2,
+      lettuce: 0.05,
+      tomato: 0.05,
+      onion: 0.05,
+      ketchup: 0.02,
+      mayo: 0.02,
     },
-    {
-      burger_name: 'Hawaiian Burger',
-      description: 'Burger with pineapple, lettuce, tomato, and onion',
-      ingredients: {
-        bread: 0.2,
-        meat: 0.2,
-        pineapple: 0.1,
-        lettuce: 0.05,
-        tomato: 0.05,
-        onion: 0.05,
-        ketchup: 0.02,
-        mayo: 0.02,
-      },
+  },
+  {
+    burger_name: 'Hawaiian Burger',
+    description: 'Burger with pineapple, lettuce, tomato, and onion',
+    photo: '/public/assets/hawaiian.jpg',
+    ingredients: {
+      bread: 0.2,
+      meat: 0.2,
+      pineapple: 0.1,
+      lettuce: 0.05,
+      tomato: 0.05,
+      onion: 0.05,
+      ketchup: 0.02,
+      mayo: 0.02,
     },
+  },
 ];
 
 //get all burgers
 router.get('/menu', async (req, res) => {
-    console.log("get all burguers view", req)
-    res.render('menu');
+    console.log("get all burgers view", req)
+    res.render('all-burgers', {hamburgers});
 });
   
 //get one burger
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 
-router.use('/', menuRoutes);
-router.use('/orders', orderRoutes);
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
