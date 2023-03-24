c02b2e928d16b78e3c0fee03dde056fe6966d7eb
Fer-117 <fsg11497@gmail.com>
2023-03-23 19:44:44 -0600

    Gráfica funcionando con graphAPI para la página de burgers

---
+++ chartJS/chart.js
@@ -0,0 +1,24 @@
+const ctx = document.getElementById("myChart");
+
+const myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "Kilograms",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+// myChart;
---
+++ signUp/script.js
@@ -0,0 +1,8 @@
+let signUpBtn = document.getElementById("signUpBtn");
+let logInBtn = document.getElementById("logInBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+
+logInBtn.addEventListener("click", () => {
+  nameField.style.maxHeight = 0;
+});
