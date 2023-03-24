f9df06f77b64104b4116a0e9cc4a112e312fd0ab
Drums180 <daviddomor180@hotmail.com>
2023-03-23 18:50:26 -0600

    corrections

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const session = require('express-session');
 const sequelize = require('./config/connection');
+// const helpers = require('./utils/helpers')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Incorporate custom handlebars (if done)
 const hbs = exphbs.create({});
 
 // Middleware
@@ -16,12 +18,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// AUTHENTICATION PENDING
+// AUTHENTICATION SESSIONS
 const crypto = require('crypto');
 const secretKey = crypto.randomBytes(64).toString('hex');
 
 app.use(session({
-  secret: secretKey,
+  secret: 'Super secret secret',
   resave: false,
   saveUninitialized: false
 }));
@@ -37,6 +39,5 @@ app.use((err, req, res, next) => {
 });
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}! Visit http://localhost:${PORT}`));
 });
-
