bc193cb71577891befff9969afea3ade8c73532d
Samuel Russek <samuel_bstone_13@hotmail.com>
2023-03-26 16:31:41 -0600

    ok2

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
+// const homeRoutes = require('./home-routes.js'); hay que eliminar home routes
+// added menu routes
 const menuRoutes = require('./menu-routes');
+// added orderRoutes
 const orderRoutes = require('./order-routes');
 
-
-router.use('/menu', menuRoutes);
-router.use('/', orderRoutes);
+router.use('/', menuRoutes);
+router.use('/orders', orderRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
