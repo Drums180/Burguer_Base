bfc04811180086fa76b03c95f9c9409b35956770
Samuel Russek <samuel_bstone_13@hotmail.com>
2023-03-26 21:33:43 -0600

    changes

--- js/login.js
+++ js/login.js
@@ -9,9 +9,12 @@ const loginFormHandler = async (event) => {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
+      
       });
   
       if (response.ok) {
+        alert (log in succesful)
+
         document.location.replace('/');
       } else {
         alert('Failed to log in.');
